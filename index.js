01b7edc2e37aac0bf82cb7aa5303de5bcd262a3b
Tonny Bett <betttonny26@gmail.com>
2024-07-21 02:51:28 +0300

    complete the lab

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = function() {
+    return 2000 / 100;
+  };
+  const square = (num) => {
+    return num * num;
+  };
+
+  const add = (num1, num2) => {
+    return num1 + num2;
+  };
+  
+  module.exports = {
+    divide,
+    square,
+    add
+  };
